10cd8784b939251520f73c72311bdaf28e420ee6
Ahmed Akhtar <32972045+ahmedakhtar11@users.noreply.github.com>
2019-01-07 23:03:43 -0600

    Update virtualassistant.rb

--- virtualassistant.rb
+++ virtualassistant.rb
@@ -1,20 +1,31 @@
 #Ahmed Akhtar
 # Virtual Assistant - Ruby
 # Under Construction
-# Example Inputs #=> Hello, Goodbye, Time
-
+# Example Inputs #=> Hello, Goodbye, Time 1+1
 time = Time.new
 
 # Getting Input
 userinput = gets.chomp
 
-case userinput
-when 'Hello'
+userinput = userinput.downcase
+
+#Calculator Setup
+num1 = userinput[0].to_f
+opr = userinput[1]
+num2 = userinput[2].to_f
+
+# Start of If Statement =>
+            
+            #Greeting
+if userinput == 'hello'
   puts "Hi! I am Oracle your Virtual Assitant."
-when 'Goodbye'
+
+            #Goodbye
+elsif userinput == 'Goodbye'
   puts "Goodbye, Have a Nice Life."
-when "Time"
-  # Components of Time
+elsif userinput == "time"
+
+      # Components of Time
 puts "Current Time: #{time.inspect}"
 puts "Year: #{time.year}" # => Year
 puts "Month: #{time.month}" # => Month
@@ -26,6 +37,21 @@ puts "Minute: #{time.min}" # => 59
 puts "Second: #{time.sec}" # => 59
 puts "Microsecond: #{time.usec}"
 puts "Time-Zone: #{time.zone}" # => "UTC"
+
+             #Calculator
+             
+elsif opr == "+"
+puts num1+num2
+
+elsif opr == "-"
+puts num1-num2
+
+elsif opr == "*"
+puts num1*num2
+
+elsif opr == "/"
+puts num1/num2
+
 else
   puts "Im Sorry, I do not understand."
 end
